fb37cf16b57288dedae4e574b194ea5cb983604c
Rakesh P <FeelCoffee@hotmail.com>
2020-04-24 17:21:59 -0400

    Updated Port

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 //now it load express module with `require` directive
 var express = require('express')
 var app = express()
+var port_number = process.env.PORT || 8080;
+const outputToWeb = "Hello Docker & Node JS on port " + port_number + "!";
+const outputToConsole = "app listening on port " + port_number + "!";
+	
 //Define request response in root URL (/) and response with text Hello World!
 app.get('/', function (req, res) {
-  res.send('Hello Docker & Node JS!')
+	res.send(outputToWeb)	
 })
 //Launch listening server on port 8080 and consoles the log.
-app.listen(8080, function () {
-  console.log('app listening on port 8080!')
+app.listen(port_number, function () {
+	console.log(outputToConsole)
 })
